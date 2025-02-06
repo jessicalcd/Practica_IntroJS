2f4df0c85719a7ab05f9fbecf48acdaa858a07b4
jessicaluzcd <jessicaluzcd@gmail.com>
2025-02-06 19:45:23 +0000

    Ejercicio 3 funciona

---
+++ ejercicio3.js
@@ -0,0 +1,30 @@
+//3.1 Ejercicio 
+
+const input1 = [
+    'Downloads',
+    'Videos',
+    'capture',
+    'mp4',
+]
+
+const input2 = [
+    'CodingGame',
+    'python',
+    'py',
+];
+
+const input3 = [
+    'programming',
+    'languages',
+    'easy',
+    'beginner',
+    'useful',
+    'pythonstuff',
+    'py',
+];
+
+const crearLinea = (list) => {
+    return list.slice(0, -1).join('/') + '/' + list[list.length - 2] + '.' + list[list.length - 1];
+};
+
+console.log(crearLinea(input3))
